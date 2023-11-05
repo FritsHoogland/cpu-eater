5690ebb9af635f26f552929e5639dad0859313f2
Frits Hoogland <frits.hoogland@gmail.com>
2023-11-05 11:11:37 +0100

    more clear formatting

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::{
-    thread, 
-    env, 
-    process, 
-    time::Instant, 
-    iter::repeat_with, 
-    sync::{Arc},
-    sync::atomic::{AtomicU64,Ordering},
+use std::{thread,
+          env,
+          process,
+          time::Instant,
+          iter::repeat_with,
+          sync::{Arc,
+                 atomic::{AtomicU64,Ordering}
+          },
 };
 use os_id::thread as osthread;
 
 const COUNTER_STEP: usize = 10_000;
 
-fn main() {
+fn main()
+{
     // collect number of threads from first argument, and panic if it doesn't exist.
     let args: Vec<String> = env::args().collect();
     let nthreads = &args[1].parse::<usize>().unwrap();
@@ -25,9 +26,11 @@ fn main() {
             .collect();
 
     let timer = Instant::now();
-    for nr in 0..*nthreads {
-        let cv_clone = counter_vector.clone();
-        threads.push(thread::Builder::new().name(format!("cpu-eater-w-{}",nr)).spawn(move || {
+    for nr in 0..*nthreads
+    {
+        let counter_vector_clone = counter_vector.clone();
+        threads.push(thread::Builder::new().name(format!("cpu-eater-w-{}",nr)).spawn(move ||
+        {
             println!("threadid: {:?}", osthread::get_raw_id());
             let mut _x=0;
             let mut loop_counter=0;
@@ -38,18 +41,20 @@ fn main() {
                 loop_counter +=1;
                 if loop_counter == COUNTER_STEP
                 {
-                    let _ = cv_clone[nr].fetch_add(1,Ordering::Relaxed);
+                    let _ = counter_vector_clone[nr].fetch_add(1,Ordering::Relaxed);
                     loop_counter = 0;
                 }
             }
         }));
     };
 
-    let _ = ctrlc::set_handler(move || {
+    let _ = ctrlc::set_handler(move ||
+    {
         println!();
         let time_spent_millis = timer.elapsed().as_millis();
         let mut total = 0;
-        for (nr, number) in counter_vector.iter().map(|number| number.load(Ordering::Relaxed)).enumerate() {
+        for (nr, number) in counter_vector.iter().map(|number| number.load(Ordering::Relaxed)).enumerate()
+        {
             total += number as u128/time_spent_millis;
             println!("thread number: {}, steps: {}, steps/ms: {}, ", nr, number as u128, number as u128/time_spent_millis);
         };
@@ -58,7 +63,8 @@ fn main() {
     });
 
 
-    for thread in threads {
+    for thread in threads
+    {
         let _ = thread.expect("Getting thread handle failed").join();
     };
 }
